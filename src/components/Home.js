39f0496a7445e3363499c1ac7ebfc276f532dee0
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2021-03-19 19:57:02 +0200

    adding list with all time classic movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { GetPopularMovies } from '../services/Fetcher'
+import { GetPopularMovies, GetTopMoviesOfAllTime } from '../services/Fetcher'
 import Header from './Header'
 import HomeMovieCard from './HomeMovieCard'
 
@@ -9,24 +9,25 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 export default function Home() {
     const [popular, setPopular] = useState([]);
+    const [topMovies, setTopMovies] = useState([]);
 
     const options = {
-        items: 6,
+        items: 5,
         margin: 15,
         slideBy: 3,
         lazyLoad: true,
-        rewind: true,
+        rewind: true
     }
 
     useEffect(() => {
-        GetPopularMovies()
-            .then(res => res.json())
+        GetPopularMovies().then(res => res.json())
             .then(data => {
-                if (!data.errors) {
-                    setPopular(data.results)
-                } else {
-                    setPopular([])
-                }
+                if (!data.errors) { setPopular(data.results) } else { setPopular([]) }
+            })
+
+        GetTopMoviesOfAllTime().then(res => res.json())
+            .then(data => {
+                if (!data.errors) { setTopMovies(data.results) } else { setTopMovies([]) }
             })
     }, [])
 
@@ -40,9 +41,22 @@ export default function Home() {
                             <h2 class="section__title">Popular Movies</h2>
                         </div>
                         <div class="col-12">
-                            <OwlCarousel options={options}>
-                                {popular.length > 0 ? popular.map(movie => <HomeMovieCard movie={movie} key={movie.id} />) : ""}
-                            </OwlCarousel>
+                            <div class="section__carousel-wrap">
+                                <OwlCarousel options={options}>
+                                    {popular.length > 0 ? popular.map(movie => <HomeMovieCard movie={movie} key={movie.id} />) : ""}
+                                </OwlCarousel>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <h2 class="section__title">All Time Classics</h2>
+                        </div>
+                        <div class="col-12">
+                            <div class="section__carousel-wrap">
+                                <OwlCarousel options={options}>
+                                    {topMovies.length > 0 ? topMovies.map(movie => <HomeMovieCard movie={movie} key={movie.id} />) : ""}
+                                </OwlCarousel>
+                            </div>
                         </div>
                     </div>
                 </div>
