009186cff470ac29d8fe40616f977e947d7238be
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2021-03-25 18:34:47 +0200

    updating pagination logic

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -110,13 +110,13 @@ export default function Category(props) {
                                                 {totalPages > 1 ?
                                                     <ul className="catalog__paginator">
                                                         <li>
-                                                            <Link to={`/categories/${genreId}/${genreName}/${currentPage > 1 ? currentPage - 1 : currentPage}`}>
+                                                            <Link to={`/categories/${genreId}/${genreName}/${currentPage > 1 ? currentPage - 1 : totalPages}`}>
                                                                 <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.75 5.36475L13.1992 5.36475" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" /><path d="M5.771 10.1271L0.749878 5.36496L5.771 0.602051" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" /></svg>
                                                             </Link>
                                                         </li>
 
                                                         <li>
-                                                            <Link to={`/categories/${genreId}/${genreName}/${currentPage + 1}`}>
+                                                            <Link to={`/categories/${genreId}/${genreName}/${currentPage < totalPages ? currentPage + 1 : 1}`}>
                                                                 <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.1992 5.3645L0.75 5.3645" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" /><path d="M8.17822 0.602051L13.1993 5.36417L8.17822 10.1271" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" /></svg>
                                                             </Link>
                                                         </li>
