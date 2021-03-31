b8b37fbaa4f8d7c9c0aa0e4e3194efef8b1b4919
unknown <68417157+tonsan221@users.noreply.github.com>
2021-03-31 13:02:11 +0300

    adding comments section to details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -64,7 +64,7 @@ export default function Details(props) {
                                     <a href="http://www.youtube.com/watch?v=0O2aH4XLbto" className="article__trailer">
                                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 1C16.5228 1 21 5.47716 21 11C21 16.5228 16.5228 21 11 21C5.47716 21 1 16.5228 1 11C1 5.47716 5.47716 1 11 1Z" stroke-linecap="round" stroke-linejoin="round"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M14.0501 11.4669C13.3211 12.2529 11.3371 13.5829 10.3221 14.0099C10.1601 14.0779 9.74711 14.2219 9.65811 14.2239C9.46911 14.2299 9.28711 14.1239 9.19911 13.9539C9.16511 13.8879 9.06511 13.4569 9.03311 13.2649C8.93811 12.6809 8.88911 11.7739 8.89011 10.8619C8.88911 9.90489 8.94211 8.95489 9.04811 8.37689C9.07611 8.22089 9.15811 7.86189 9.18211 7.80389C9.22711 7.69589 9.30911 7.61089 9.40811 7.55789C9.48411 7.51689 9.57111 7.49489 9.65811 7.49789C9.74711 7.49989 10.1091 7.62689 10.2331 7.67589C11.2111 8.05589 13.2801 9.43389 14.0401 10.2439C14.1081 10.3169 14.2951 10.5129 14.3261 10.5529C14.3971 10.6429 14.4321 10.7519 14.4321 10.8619C14.4321 10.9639 14.4011 11.0679 14.3371 11.1549C14.3041 11.1999 14.1131 11.3999 14.0501 11.4669Z" stroke-linecap="round" stroke-linejoin="round"></path></svg>
 							Trailer
-						</a>
+						            </a>
 
                                     <div className="article__content">
                                         <h1>{movieDetails.title}</h1>
@@ -98,6 +98,42 @@ export default function Details(props) {
                                                 {genre.name}
                                             </Link>
                                         )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 col-xl-8">
+                                <div class="comments">
+                                    <ul class="nav nav-tabs comments__title comments__title--tabs" id="comments__tabs" role="tablist">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" data-toggle="tab" href="#tab-1" role="tab" aria-controls="tab-1" aria-selected="true">
+                                                <h4>Comments</h4>
+                                                <span>5</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div class="tab-content">
+                                        <div class="tab-pane fade show active" id="tab-1" role="tabpanel">
+                                            <ul class="comments__list">
+                                                <li class="comments__item">
+                                                    <div class="comments__autor">
+                                                        <img class="comments__avatar" src="img/avatar.svg" alt="" />
+                                                        <span class="comments__name">Brian Cranston</span>
+                                                        <span class="comments__time">30.08.2021, 17:53</span>
+                                                    </div>
+                                                    <p class="comments__text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+                                                </li>
+                                            </ul>
+
+                                            <form class="comments__form">
+                                                <div class="sign__group">
+                                                    <textarea id="text" name="text" class="sign__textarea" placeholder="Add comment"></textarea>
+                                                </div>
+                                                <button type="button" class="sign__btn">Send</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
