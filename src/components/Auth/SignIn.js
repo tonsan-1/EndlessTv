a63b6c6c70f07d098cd8410ca427344dc20b7f05
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2021-04-01 13:54:15 +0300

    success and error messages fixed in signIn

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { useAuth } from '../../services/Auth'
-import { Alert } from 'react-bootstrap'
+import SweetAlert from 'react-bootstrap-sweetalert';
 import './Sign.css'
 
 export default function SignUp() {
@@ -9,6 +9,7 @@ export default function SignUp() {
     const passwordRef = useRef();
     const { login } = useAuth();
     const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
     const [loading, setLoading] = useState(false);
     const history = useHistory();
 
@@ -19,24 +20,34 @@ export default function SignUp() {
         login(emailRef.current.value, passwordRef.current.value)
             .then(res => {
                 setError('');
+                setSuccess(true);
                 setLoading(true);
-                history.push('/');
+
+                setTimeout(() => {
+                    history.push('/')
+                }, 1600)
             })
             .catch(error => {
                 setError(error.message);
+
+                setTimeout(() => {
+                    setError('');
+                }, 2000)
             });
 
         setLoading(false);
     }
     return (
         <div className="sign section--full-bg">
+            {error && <SweetAlert showConfirm={false} danger title={error}/>}
+            {success && <SweetAlert showConfirm={false} success title="You signed in successfully!"/>}
+
             <div className="container">
                 <div className="row">
                     <div className="col-12">
                         <div className="sign__content">
                             <form onSubmit={handleSubmit} className="sign__form">
                                 <a href="index.html" className="sign__logo">EndlessTV</a>
-                                {error && <Alert variant="danger">{error}</Alert>}
                                 <div className="sign__group">
                                     <input type="email" ref={emailRef} required className="sign__input" placeholder="Email" />
                                 </div>
