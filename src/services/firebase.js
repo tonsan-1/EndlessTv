5f0e6c518fea18a45e3e1f01f81c1c2f7b70f18a
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2021-04-08 19:22:05 +0300

    firebase api key update

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,13 @@ import 'firebase/auth'
 
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDPgmkBf2qCiWivIybjDbD_RUcnKOwkWcw",
+    authDomain: "endlesstv.firebaseapp.com",
+    databaseURL: "https://endlesstv-default-rtdb.firebaseio.com",
+    projectId: "endlesstv",
+    storageBucket: "endlesstv.appspot.com",
+    messagingSenderId: "673035889001",
+    appId: "1:673035889001:web:ffb3befcde496b4ba4b5f8"
 })
 
 export const auth = app.auth();
